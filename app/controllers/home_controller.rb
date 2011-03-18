5f4b73f4c121bf38c1126ed5a43793f344c2163a
unknown <ppaasch@.(none)>
2011-03-17 23:32:13 -0500

    removing logging

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-   logger.debug("HERE")
-   logger.debug(params[:id])
    @team = Team.find(params[:id])
    @upcoming_matches = Match.includes(:opponent).where("team_id = ? AND match_date >= ?", @team.id, Date.today).limit(5)
   end
